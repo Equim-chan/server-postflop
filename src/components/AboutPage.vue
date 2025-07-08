08aa2086dcabd8e92f00c7b090d4174d926e06a7
Equim <sayaka@ekyu.moe>
2025-07-08 23:46:09 +0900

    add README

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -1,13 +1,13 @@
 <template>
-  <p>Desktop Postflop v0.2.7</p>
+  <p>Server Postflop v0.2.7</p>
   <p>Copyright (C) 2022 Wataru Inariba</p>
   <p>
     <a
       target="_blank"
       class="link"
-      href="https://github.com/b-inary/desktop-postflop"
+      href="https://github.com/Equim-chan/server-postflop"
     >
-      https://github.com/b-inary/desktop-postflop
+      https://github.com/Equim-chan/server-postflop
     </a>
   </p>
 </template>
